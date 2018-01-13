6bdf289d6d701b89238414d3aac071c46a5f158a
Kaue Pereira <kaue@MacBook-Pro-de-Kaue.local>
2018-01-13 19:40:56 -0400

    Using API, Restaurant services created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from 'app/home/home.component';
 import { AboutComponent } from 'app/about/about.component';
+import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},
     {path: 'about', component: AboutComponent}
 ];
