462adf3d1ca8ecddb6ea41876b2400cb8adc4294
Kaue Pereira <kaue@MacBook-Pro-de-Kaue.local>
2018-01-14 01:54:01 -0400

    restaurants detail routes and pages created

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,1 @@
-export const MEAT_API = 'http://localhost:3000gi'
+export const MEAT_API = 'http://localhost:3000'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from 'app/home/home.component';
 import { AboutComponent } from 'app/about/about.component';
 import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
+import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent},
     {path: 'about', component: AboutComponent}
 ];
